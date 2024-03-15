bf8bab21a714f2f084d6c7504b5fccca708fab6a
Timothy Dewanto <dewantotims@gmail.com>
2024-03-15 09:39:23 +0700

    first

---
+++ AdvWeek4160421125/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AdvWeek4160421125"
+include(":app")
+ 
