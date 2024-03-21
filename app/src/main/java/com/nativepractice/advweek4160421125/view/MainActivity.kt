43a85a43bdefe6f8ddc44e3320ad139eb8e33d9f
Timothy Dewanto <dewantotims@gmail.com>
2024-03-21 07:04:24 +0700

    done

--- app/src/main/java/com/nativepractice/advweek4160421125/view/MainActivity.kt
+++ app/src/main/java/com/nativepractice/advweek4160421125/view/MainActivity.kt
@@ -3,10 +3,13 @@ package com.nativepractice.advweek4160421125.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nativepractice.advweek4160421125.R
+import com.nativepractice.advweek4160421125.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
