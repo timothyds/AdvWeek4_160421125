7068a0f49bb9ba2b9f9eb31111ba596a1feca93c
Timothy Dewanto <dewantotims@gmail.com>
2024-03-21 12:40:02 +0700

    tugasW4

--- app/src/main/java/com/nativepractice/advweek4160421125/view/StudentDetailFragment.kt
+++ app/src/main/java/com/nativepractice/advweek4160421125/view/StudentDetailFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nativepractice.advweek4160421125.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.nativepractice.advweek4160421125.databinding.FragmentStudentDetailBinding
+import com.nativepractice.advweek4160421125.viewmodel.DetailViewModel
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel:DetailViewModel
+    private lateinit var binding:FragmentStudentDetailBinding
+    fun observeStudent() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
 
+            binding.txtId.setText(viewModel.studentLD.value?.id)
+            binding.txtStudentName.setText(viewModel.studentLD.value?.name)
+            binding.txtBoD.setText(viewModel.studentLD.value?.dob)
+            binding.txtPhone.setText(viewModel.studentLD.value?.phone)
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        binding = FragmentStudentDetailBinding.inflate(inflater, container, false)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeStudent()
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
